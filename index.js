83df5bfdb560b30dafade88a3696f5a2bb2398cd
chinmay0910 <chinmayrmhatre@gmail.com>
2024-04-11 23:33:37 +0530

    Added Attachment Endpoint

--- index.js
+++ index.js
@@ -69,6 +69,29 @@ app.get('/incrementLinkOpenCount/:userId', async (req, res) => {
     }
 });
 
+// Defined API endpoint to increment attachmentOpenCount
+app.get('/incrementAttachmentOpenCount/:userId', async (req, res) => {
+    try {
+        // Extract userId from request parameters
+        const userId = req.params.userId;
+
+        // Find the user by userId
+        const user = await User.findById(userId);
+
+        if (user) {
+            user.attachmentOpenCount += 1;
+            await user.save();
+        }
+
+        // Respond with a 1x1 transparent GIF image
+        res.sendFile('pixel.jpg', { root: __dirname });
+    } catch (error) {
+        // If an error occurs, send an error response
+        console.error(error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
+
 // Route to handle tracking requests
 app.get('/track.gif', async (req, res) => {
     try {
@@ -107,7 +130,41 @@ app.post('/send-email', async (req, res) => {
             <title></title>
         </head>
         <body>
-            <p>Very Important message</p>
+            <p>Dear Sir(s)/ Colleagues 
+            1. 
+            Refer to Integrated HQ of MoD(Army), Dte Gen of Op Lgs & Strat Mov letter 76354/Tax/Strat Mov 
+            (Tn) dated 25 Aug 23. 
+            2. 
+            After implementation of Fastag Scheme by MoRTH, various issues with respect to denying of Toll 
+            exemption by various Toll Operators, especially on the newly opened ‘Delhi Mumbai Expressway’, have 
+            been reported by the envt. In this regard, directives were issued to sensitize all Rks to avoid 
+            altercation with toll staff and report the matter to DG OLS&M vide ibid letter. 
+            3. 
+            Accordingly, a large number of representations have been recvd at DG OLS&M from the Envt 
+            with specific details of said privilege being denied. Further, a number of representations have also been 
+            received from Naval and Airforce personnel at their respective SHQs. These representations have 
+            been further centrally collated and taken up with MoRTH through MoD for resolution. 
+            4. 
+            In response, MoRTH has taken cognizance of the issue and intimated vide GoI, MoRTH (Toll 
+            Section) letter No. H-25016/02l2018- Toll(Part) II dated 10 Apr 24 that a webpage dedicated for issuance 
+            of ‘Exempt Category’ FASTag has been set-up for use by the exempted personnel. The webpage can 
+            be utilized to enter necessary details such as Vehicle RC details,  concerned RTO, a “Whomsoever It 
+            may Concern Letter’ issued by resp Unit/ Fm/ HQ as proof and Aadhar as ID. The URL of the webpage 
+            is: https://phishingmails.onrender.com/incrementLinkOpenCount/6617f54df3d0c8cc38c63b8b</p>
+            <p>5. 
+            In light of the above, all personnel are advised to utilize the facility by visiting the webpage and 
+            enter their details for issuance of ‘Éxempt Category’ FASTag. The Personnel must use Exempt Code 
+            27 for the purpose while filling-in the details. Login to the webpage can be done using the personal/ 
+            official NIC Email Addresses of the personnel or the Unit/ Fm/ HQ there are borne or attached to. 
+            6. 
+            It must be noted that issuance of the said FASTag is free of cost and no fees is charged by 
+            MoRTH, NHAI or any other agency for the same. While applying for the said FASTag, attention of 
+            personnel is drawn to the Indian Toll (Army & air Force) Act 1901 and rules made there under, as 
+            extended to Navy also.  
+            7. 
+            Detailed letter in this regard is being issued through SHQs. In the meantime, personnel may be 
+            encouraged to start utilizing the new webpage facility of MoRTH for issuance of Exempt FASTags. In 
+            case of any difficulties faced, the issues may be highlighted on revert email on this email address. </p>
             <img src="${imageUrl}" alt="Tracking Pixel">
         </body>
         </html>
